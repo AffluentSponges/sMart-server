47d6f79afa5ece4073c7968bd94d98c6f520b21f
Brenner Spear <blspear@gmail.com>
2017-02-17 20:02:35 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+// var router = require('./routes');
+// var passport = require('./server/passport');
+
+//use .env for port number
+var port = 8080;
+
+var app = express();
+
+
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+// app.use(require('express-session')({ secret: 'so smart of you', resave: true, saveUninitialized: true }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use('', router);
+
+app.listen(port, () => {
+  console.log('listening on port ', port);
+});
+
+module.exports = app;
