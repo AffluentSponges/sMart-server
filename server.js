4a1c27d0114aba1477fe032d2fbb5cc2ad98eb68
Daniel Voigt <danielralphvoigt@gmail.com>
2017-02-20 12:02:15 -0800

    Initialize passport session

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const db = require('./db/db');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 // var router = require('./routes');
 // var passport = require('./server/passport');
@@ -14,13 +16,17 @@ const app = express();
 
 app.use(morgan('combined'));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(require('express-session')({ secret: 'so smart of you', resave: true, saveUninitialized: true }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.use('', router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(express.static(path.join(__dirname, '..', '..', 'client')));
 
 app.listen(port, () => {
   console.log('listening on port ' + port);
