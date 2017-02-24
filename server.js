276b49a17839edfc2d6c588d80e3bec75d5798ea
Mark <jungtaehoon@icloud.com>
2017-02-24 12:35:06 -0800

    ready to merge

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET_KEY || 'nothing is secret',
   resave: false,
   saveUninitialized: true,
-  // store: new FileStore(),
+  store: new FileStore(),
   cookie : { maxAge: 2419200000 }
 }));
 app.use(passport.initialize());
