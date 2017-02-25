fb7d468ba630c3214cddd79f44a971b38cea9876
Brenner Spear <blspear@gmail.com>
2017-02-24 16:52:08 -0800

    Add 2 more GET route tests

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -5,13 +5,13 @@ var model = {}
 model.addNewTransaction = function(product) {
   var date = new Date()
   date = date.toUTCString()
- return db.Transaction.upsert({product_id: product.id},
+  return db.Transaction.upsert({product_id: product.id},
     {
-    user_id: product.attributes.buyer_id,
-    sale_price: product.attributes.asking_price,
-    status: 'processing_buyer_payment',
-    sale_time_and_date: date
-  })
+      user_id: product.attributes.buyer_id,
+      sale_price: product.attributes.asking_price,
+      status: 'processing_buyer_payment',
+      sale_time_and_date: date
+    })
 }
 
 
