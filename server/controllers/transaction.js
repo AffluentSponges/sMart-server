69b73c298d4b3f58e365086ac78c8d62fc2ff64f
Daniel Voigt <danielralphvoigt@gmail.com>
2017-03-06 12:19:09 -0800

    refactor

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -18,14 +18,9 @@ module.exports.deliverNotifications = function(delivery_id, status) {
     var buyer = transactionData.relations.buyer.attributes;
     var transaction = transactionData.attributes;
 
-<<<<<<< HEAD
-      	transactionData.set({status: status}).save();
-            twilio.sendSms(seller.phone_number, `S-Mart Alert to ${seller.username}: Your recently sold product, ${product.title}, is ${status.split('_').join(' ')}.\nETA: ${pickupEta}`);
-            twilio.sendSms(buyer.phone_number, `S-Mart Alert to ${buyer.username}: Your recently purchased product, ${product.title}, is ${status.split('_').join(' ')}.\nETA: ${dropoffEta}`);          
-
-=======
     transactionData.set({status: status}).save();
->>>>>>> ecacaa1bf9f19ffd550cfc1af348039ee739dd88
+    twilio.sendSms(seller.phone_number, `S-Mart Alert to ${seller.username}: Your recently sold product, ${product.title}, is ${status.split('_').join(' ')}.\nETA: ${pickupEta}`);
+    twilio.sendSms(buyer.phone_number, `S-Mart Alert to ${buyer.username}: Your recently purchased product, ${product.title}, is ${status.split('_').join(' ')}.\nETA: ${dropoffEta}`);          
 
     // uber.get(`${delivery_id}`)
     // .then(function(res) {
@@ -49,14 +44,14 @@ module.exports.deliverNotifications = function(delivery_id, status) {
 module.exports.completeTransaction = function(delivery_id) {
 	Transaction.getTransactionInfo(delivery_id)
 	.then(function(transactionData) {
-      	var transaction = transactionData.attributes;
-      	var idem = transaction.id;
-      	var seller = transactionData.relations.seller.attributes;
-      	var sellerWalletAddress = seller.wallet_address;
-      	var product = transactionData.relations.product.attributes;
-      	var salePrice = Number(product.asking_price);
+    var transaction = transactionData.attributes;
+    var idem = transaction.id;
+    var seller = transactionData.relations.seller.attributes;
+    var sellerWalletAddress = seller.wallet_address;
+    var product = transactionData.relations.product.attributes;
+    var salePrice = Number(product.asking_price);
 		
-            coinbaseController.convertCurrency(salePrice)
+    coinbaseController.convertCurrency(salePrice)
 		.then(function(btc) {
 			coinbaseController.sendBTC(idem, sellerWalletAddress, btc);
 		})
