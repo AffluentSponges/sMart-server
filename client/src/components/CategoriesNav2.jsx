202a6a06d36bf82854c3974766c290df00dee5cf
Taehoon Jung <jungtaehoon@icloud.com>
2017-03-08 21:55:53 -0800

    menu is mobile responsive (#127)
    * change background image
    
    * menu is responsive

--- client/src/components/CategoriesNav2.jsx
+++ client/src/components/CategoriesNav2.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Segment, Divider, Menu } from 'semantic-ui-react'
+import Dimensions from 'react-dimensions'
 
 class CategoriesNav2 extends React.Component {
   constructor(props) {
@@ -13,16 +14,18 @@ class CategoriesNav2 extends React.Component {
 
   render() {
     const activeItem = this.props.state.currentCategoryName;
+    var vertical = false;
+    if (this.props.containerWidth < 1000) {
+      vertical = true;
+    }
     return (
-      <div>
-        <Menu pointing secondary stackable color='red' className='CategoriesNav2'>
-          {this.props.state.categories.map((category)=>{
-            return <Menu.Item as={Link} name={category.name} to={`/x/${category.id}`} key={category.id} active={activeItem === category.name} onClick={this.handleItemClick} />
-          })}
-        </Menu>
-      </div>
+      <Menu pointing secondary vertical={vertical} fluid color='red' className='CategoriesNav2'>
+        {this.props.state.categories.map((category)=>{
+          return <Menu.Item as={Link} name={category.name} to={`/x/${category.id}`} key={category.id} active={activeItem === category.name} onClick={this.handleItemClick} />
+        })}
+      </Menu>
     )
   }
 }
 
-export default CategoriesNav2;
+export default Dimensions()(CategoriesNav2);
