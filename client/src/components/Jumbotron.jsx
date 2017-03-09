67a5d674dca9094b68d1d046b963a70ea61a9d18
Mark <jungtaehoon@icloud.com>
2017-03-08 22:09:57 -0800

    bug fix

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -9,42 +9,25 @@ class Jumbotron extends React.Component {
   }
 
   render() {
-    console.log()
+
     return (
-      <div>
-        <div className='jumbotron'>
-          <div className='space'></div>
-          <Grid centered>
-            <Grid.Column width='12'>
-                <h1>FIND GREAT DEALS IN SF,</h1>
-                <h1>AND HAVE IT <span>IN 30 MINS</span> IN YOUR HOME</h1>
+      <div className='jumbotron'>
+        <div className='space'></div>
+        <Grid centered>
+          <Grid.Column width='12'>
+              <h1>FIND GREAT DEALS IN SF,</h1>
+              <h1>AND HAVE IT <span>IN 30 MINS</span> IN YOUR HOME</h1>
+          </Grid.Column>
+          <Grid.Row centered columns={3}>
+            <Grid.Column> 
             </Grid.Column>
-            <Grid.Row centered columns={3} only='computer'>
-              <Grid.Column> 
-              </Grid.Column>
-              <Grid.Column width={4}>
-                <Search searchData={this.props.searchData} size='huge' className='search'/>
-              </Grid.Column>
-              <Grid.Column> 
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-        <div>
-          <Segment>
-            <Grid>
-              <Grid.Row centered columns={3} only='mobile'>
-                <Grid.Column> 
-                </Grid.Column>
-                <Grid.Column width={4}>
-                  <Search searchData={this.props.searchData} size='huge' className='search'/>
-                </Grid.Column>
-                <Grid.Column> 
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
-        </div>
+            <Grid.Column width={4}>
+              <Search searchData={this.props.searchData} size='huge' className='search'/>
+            </Grid.Column>
+            <Grid.Column> 
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
